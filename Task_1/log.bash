74f9fdd9ae66c5056cd695a63b5570515237f78a
Barbara-S5 <vsankova398@gmail.com>
2026-02-28 11:55:51 +0000

    fin

--- Task_1/log.bash
+++ Task_1/log.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-awk '$10 ~ /^[0-9]+$/ {sum[$1]+=$10} END {for (ip in sum) if (sum[ip] >= 0) printf "%s bytes for %s\n", sum[ip], ip}' access.log | sort -bnr > test.txt
+awk '$10 ~ /^[0-9]+$/ {sum[$1]+=$10} END {for (ip in sum) if (sum[ip] >= 0) printf "%s bytes for %s\n", sum[ip], ip}' access.log | sort -bn
